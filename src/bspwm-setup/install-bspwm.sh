6e98501fd6fad0a7a786e0189fb7baf1d52e17d5
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2022-04-15 20:28:59 -0300

    Modify bspwm installation script. Now only installs bspwm.

--- src/bspwm-setup/install-bspwm.sh
+++ src/bspwm-setup/install-bspwm.sh
@@ -1,22 +1,19 @@
 #!/bin/bash     
 
 # 1. Actualizamos el sistema.
-sudo apt update
-sudo apt upgrade
+sudo dnf update
+sudo dnf upgrade
 
 # 2. Instalamos
-sudo apt install -y bspwm sxhkd
+sudo dnf install -y bspwm
 
 # 3. Creamos los directios para almacenar la configuracion
 mkdir ~/.config/bspwm
 mkdir ~/.config/bspwm/scripts/
-mkdir ~/.config/sxhkd
 
 # 4. Copiamos los archivos de configuración
 cp resources/bspwmrc ~/.config/bspwm/
 chmod +x ~/.config/bspwm/bspwmrc
-
-cp resources/sxhkdrc ~/.config/sxhkd/
 
 cp resources/bspwm_resize ~/.config/bspwm/scripts/
 chmod +x ~/.config/bspwm/scripts/bspwm_resize
