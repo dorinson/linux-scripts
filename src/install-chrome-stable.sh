7ce203ab64c84a2a3fb70eb1e6e902b1995da296
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2022-08-03 09:55:33 -0300

    Create script for Google Chrome Stable installation.

---
+++ src/install-chrome-stable.sh
@@ -0,0 +1,3 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo dnf install -y ./google-chrome-stable_current_x86_64.rpm
+rm google-chrome-stable_current_x86_64.rpm
