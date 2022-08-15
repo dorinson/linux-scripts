550d9cf34a6aa0a3364702178483a4b1f3a1505c
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2022-08-14 23:40:04 -0400

    Add ulauncher installation and customization scripts

---
+++ src/install-ulauncher.sh
@@ -0,0 +1,5 @@
+sudo dnf install -y ulauncher
+mkdir ~/.config/ulauncher
+mkdir ~/.config/ulauncher/user-themes
+git clone https://github.com/tjquillan/Arc-Dark-ULauncher ~/.config/ulauncher/user-themes/Arc-
+cp ulauncher-settings/* ~/.config/ulauncher/
