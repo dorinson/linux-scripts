3415baa6cf4b52767bcdfc15f45055282f1aee89
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2022-12-18 22:46:36 -0300

    Update script file

--- src/install-common-apps.sh
+++ src/install-common-apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash     
 
-sudo dnf install -y git zsh wget curl neovim htop figlet lolcat neofetch exa qbittorrent tmux asciinema emacs
+sudo dnf install -y zsh wget curl neovim htop figlet lolcat neofetch exa qbittorrent tmux asciinema
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
