2fdcfab8d81bc9af36ca2ab2086d76c4f7622921
German Rodrigo Alvarez <geralvarez.085@gmail.com>
2024-05-09 23:21:55 -0300

    Rename and update main script

--- src/install-common.sh
+++ src/install-common.sh
@@ -1,9 +1,12 @@
 #!/bin/bash     
 
-sudo dnf install -y zsh wget curl neovim htop figlet lolcat neofetch exa bat procs fd-find qbittorrent tmux asciinema
+sudo dnf install -y zsh wget curl neovim htop figlet lolcat eza bat procs fd-find qbittorrent tmux asciinema
 
 # OH MY ZSHELL
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # P10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#PAPIRUS ICON THEME
+sudo dnf install -y papirus-icon-theme
